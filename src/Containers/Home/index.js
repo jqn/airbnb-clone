0ce317bcddbc4f15e1484912b0640006c890bebb
jqn <jqngl@icloud.com>
2020-10-17 14:46:04 +0200

    Added basic header and home containers

---
+++ src/Containers/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Home = () => {
+  return <div className="" />;
+};
+
+Home.defaultProps = {};
+
+Home.propTypes = {};
+
+export default Home;
